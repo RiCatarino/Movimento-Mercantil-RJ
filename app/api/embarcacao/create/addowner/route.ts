b8c6eae3d433b8c9f0f3df6c33a7f9ab3807080e
jose.carreira <jlncarreira@gmail.com>
2024-04-07 20:40:44 +0100

    addowner API

---
+++ app/api/embarcacao/create/addowner/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/lib/prisma";
+import dayjs from "dayjs";
+
+export async function POST(req: Request) {
+  const { pessoa, data_inicio, data_fim, pais, embarcacao } = await req.json();
+  const result = await prisma.relac_embarcacao_proprietario.create({
+    data: {
+      embarcacao: {
+        connect: {
+          id: Number(embarcacao),
+        },
+      },
+      pessoa: {
+        connect: {
+          id: Number(pessoa),
+        },
+      },
+      data_inicio: dayjs(data_inicio, "DD/MM/YYYY").toDate(),
+      data_fim: dayjs(data_fim, "DD/MM/YYYY").toDate(),
+      pais: {
+        connect: {
+          id: Number(pais),
+        },
+      },
+    },
+  });
+  return Response.json(result);
+}
