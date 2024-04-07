debc9a9e58ddb47228bcbdcba2ebe7a31bd091a3
Antonio Palma <antoniopalma9999@gmail.com>
2024-04-07 16:09:25 +0100

    Alterações

--- app/admin/embarcacoes/_components/addownerbutton.tsx
+++ app/admin/embarcacoes/_components/addownerbutton.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -6,21 +6,45 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@/components/ui/dialog';
-import useSWR from 'swr';
-import fetcher from '@/lib/fetch';
-import { useState } from 'react';
+} from "@/components/ui/dialog";
+import useSWR from "swr";
+import fetcher from "@/lib/fetch";
+import { useState } from "react";
+import { z } from "zod";
+
+const formSchema = z.object({
+  pessoa: z.string().min(1, { message: "Selecione uma pessoa" }),
+  data_inicio: z.date({ required_error: "Selecione uma data de início" }),
+  data_fim: z.date({ required_error: "Selecione uma data de fim" }),
+  pais: z.string().min(1, { message: "Selecione um país" }),
+});
 
 export default function AddOwner(props: { mutate: () => void }) {
   const { mutate } = props;
   const [open, setOpen] = useState(false);
   const [submitting, setSubmitting] = useState(false);
 
-  const { data: tiposEmbarcacao, isLoading } = useSWR<TipoEmbarcacao[]>(
-    '/api/embarcacao/read/tipos',
+  const { data: pessoas, isLoading } = useSWR<Pessoa[]>(
+    "/api/embarcacao/read/tipos",
     fetcher
   );
 
+  async function handleSubmit(values: z.infer<typeof formSchema>) {
+    setSubmitting(true);
+    const result = await fetch("/api/pessoa/create", {
+      method: "POST",
+      body: JSON.stringify(values),
+    });
+
+    if (result.ok) {
+      setOpen(false);
+      form.reset();
+      mutate();
+      toast.success("Pessoa criada com sucesso");
+    } else {
+      toast.error("Erro ao criar pessoa");
+    }
+  }
   //   async function handleSubmit(values: z.infer<typeof formSchema>) {
   //     setSubmitting(true);
   //     const result = await fetch('/api/embarcacao/create', {
@@ -42,13 +66,13 @@ export default function AddOwner(props: { mutate: () => void }) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button className='rounded-xl bg-blue-500'>
+        <Button className="rounded-xl bg-blue-500">
           Adicionar Proprietário
         </Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Criar Embarcação</DialogTitle>
+          <DialogTitle>Adicionar Proprietário</DialogTitle>
           <DialogDescription asChild>
             {/* <Form {...form}>
              
