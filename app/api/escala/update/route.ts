e8d359fcda3411b1c1d3f8d633e18e94e2026fb1
jose.carreira <jlncarreira@gmail.com>
2024-04-13 10:08:47 +0100

    atualização de API's

--- app/api/escala/update/route.ts
+++ app/api/escala/update/route.ts
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 
-export async function UPDATE(req: Request) {
+export async function PUT(req: Request) {
   const {
     id,
     id_viagem,
--- app/api/imagem_embarcacao/update/route.ts
+++ app/api/imagem_embarcacao/update/route.ts
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 
-export async function UPDATE(req: Request) {
+export async function PUT(req: Request) {
   const { id, imagem, id_tipo_embarcacao } = await req.json();
   const result = await prisma.imagem_embarcacao.update({
     where: {
