2b41339adff396a244c5a0ef1bf3fc78ba29fd28
jose.carreira <jlncarreira@gmail.com>
2024-04-14 20:11:57 +0100

    Adicionado API's de Update e Delete a várias tabelas

---
+++ app/api/viagem/delete/route.ts
@@ -0,0 +1,10 @@
+import prisma from "@/lib/prisma";
+
+export async function DELETE(req: Request) {
+  const { id } = await req.json();
+
+  const post = await prisma.viagem.delete({
+    where: { id },
+  });
+  return Response.json(post);
+}
