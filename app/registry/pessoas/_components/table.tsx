03bce41fc6140d7edebd3a0824078e845fb30a35
Antonio Palma <antoniopalma9999@gmail.com>
2024-04-30 12:31:50 +0100

    consulta registry de pessoas, embarcacoes e viagens já disponivel para consulta, botão de pesquisa adicionado

---
+++ app/registry/pessoas/_components/table.tsx
@@ -0,0 +1,72 @@
+'use client';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+// import VesselDetails from './vesseldetails';
+import { useState } from 'react';
+import PersonDetails from './persondetails';
+import fetcher from '@/lib/fetch';
+import Loader from '@/components/loader';
+import useSWR from 'swr';
+
+export function PeopleTable() {
+  const [open, setOpen] = useState(false);
+  const [pessoa_id, setPessoaId] = useState<number | undefined>();
+  const {
+    data: pessoas,
+    isLoading,
+    mutate,
+  } = useSWR<Pessoa[]>('/api/pessoa/read', fetcher);
+
+  if (isLoading) return <Loader classProp='w-24 h-24 self-center' />;
+  return (
+    <div className='rounded-md'>
+      <Table>
+        <TableHeader className='p-2 text-xs border-t-0 bg-gradient-to-r from-blue-200 to-blue-400 '>
+          <TableRow className='rounded-ss-xl'>
+            <TableHead>ID</TableHead>
+            <TableHead>Nome</TableHead>
+            <TableHead>Título Nobreza</TableHead>
+            <TableHead>País</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {pessoas?.map((pessoa) => (
+            <TableRow
+              className='cursor-pointer hover:bg-blue-100'
+              key={pessoa.id}
+              onClick={(e) => {
+                e.stopPropagation();
+                setPessoaId(pessoa.id);
+                setOpen(true);
+              }}
+            >
+              <TableCell className='text-xs font-medium'>{pessoa.id}</TableCell>
+              <TableCell className='text-xs font-medium'>
+                {pessoa.nome}
+              </TableCell>
+              <TableCell className='text-xs font-medium'>
+                {pessoa?.titulo_nobreza?.titulo}
+              </TableCell>
+              <TableCell className='text-xs font-medium'>
+                {pessoa.pais?.pais}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <PersonDetails
+        open={open}
+        setOpen={setOpen}
+        pessoa_id={pessoa_id}
+        mutate={mutate}
+      />
+    </div>
+  );
+}
--- app/registry/pessoas/page.tsx
+++ app/registry/pessoas/page.tsx
@@ -1,13 +1,13 @@
-import TripsTable from "./_components/table";
-import Search from "./_components/search";
+import { PeopleTable } from "../pessoas/_components/table";
+import Search from "../viagens/_components/search";
 
-export default function ViagensTable() {
+export default function PessoasPage() {
   return (
-    <main className="min-h-full flex flex-col">
+    <main className="h-full flex flex-col">
       <div className="min-h-full p-4 border-2 shadow-lg mt-24 gap-2 rounded-3xl md:mx-24">
         <div className="w-full min-h-full dark:bg-black md:h-screen md:mt-0">
           <Search />
-          <TripsTable />
+          <PeopleTable />
         </div>
       </div>
     </main>
