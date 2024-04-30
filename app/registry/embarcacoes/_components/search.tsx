7a9c65d489f9bc3b96e3579d5107aa7d5ef559b6
Antonio Palma <antoniopalma9999@gmail.com>
2024-04-30 13:14:23 +0100

    Pequenas modificações

---
+++ app/registry/embarcacoes/_components/search.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search as SearchIcon } from "lucide-react";
+
+interface SearchProps {
+  children?: React.ReactNode;
+}
+
+export default function Search({ children }: SearchProps) {
+  return (
+    <div className="flex items-center justify-left p-2 pb-3 sm:w-1/2 md:w-1/3">
+      <Input type="search" placeholder="Pesquisa . . ." />
+      <div>
+        <Button className="text-white shadow-2xl transition-all duration-300 bg-blue-500 rounded-m hover:scale-105 hover:bg-blue-200 active:scale-95 ml-2">
+          <SearchIcon />
+        </Button>
+      </div>
+      {children}
+    </div>
+  );
+}
