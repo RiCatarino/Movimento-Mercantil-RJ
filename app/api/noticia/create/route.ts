c900ca0541cbe9d72a7e22a6dad9f89ddba9b191
jose.carreira <jlncarreira@gmail.com>
2024-05-03 21:08:51 +0100

    API referencia_documental

--- app/api/noticia/create/route.ts
+++ app/api/noticia/create/route.ts
@@ -1,7 +1,7 @@
-import prisma from '@/lib/prisma';
+import prisma from "@/lib/prisma";
 
 export async function POST(req: Request) {
-  const { id_viagem, assunto, viagem } = await req.json();
+  const { id_viagem, assunto } = await req.json();
   const result = await prisma.noticia.create({
     data: {
       viagem: {
--- app/api/referencia_documental/create/route.ts
+++ app/api/referencia_documental/create/route.ts
@@ -1,10 +1,17 @@
-import prisma from '@/lib/prisma';
+import prisma from "@/lib/prisma";
 
 export async function POST(req: Request) {
-  const { nome_periodico } = await req.json();
+  const { nome_periodico, id, date, id_viagem } = await req.json();
   const result = await prisma.referencia_documental.create({
     data: {
       nome_periodico: nome_periodico,
+      relacao_viagem_referencia_doc: {
+        connect: {
+          id: id,
+          data_publicacao: date,
+          id_viagem: id_viagem,
+        },
+      },
     },
   });
   return Response.json(result);
