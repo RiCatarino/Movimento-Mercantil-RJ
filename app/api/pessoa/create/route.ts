ea04ff7851eb76010f1f172a251936ae650e9a3f
jose.carreira <jlncarreira@gmail.com>
2024-05-05 21:06:43 +0100

    Issue #18 resolvido, verifica se a pessoa existe na DB por nome, pais e título nobreza

--- app/api/pessoa/create/route.ts
+++ app/api/pessoa/create/route.ts
@@ -2,6 +2,29 @@ import prisma from "@/lib/prisma";
 
 export async function POST(req: Request) {
   const { nome, pais, titulo_nobreza } = await req.json();
+
+  const existe = await prisma.pessoa.findFirst({
+    where: {
+      nome: {
+        equals: nome,
+        mode: "insensitive",
+      },
+      pais: {
+        id: Number(pais),
+      },
+      titulo_nobreza: {
+        id: Number(titulo_nobreza),
+      },
+    },
+  });
+
+  if (existe) {
+    return new Response("Título de Nobreza já existe", {
+      status: 409,
+      statusText: "Título de Nobreza já existe",
+    });
+  }
+
   const result = await prisma.pessoa.create({
     data: {
       nome: nome,
