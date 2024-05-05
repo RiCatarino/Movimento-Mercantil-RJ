147496557e381142a7d8187808991fa2b7f1d30a
jose.carreira <jlncarreira@gmail.com>
2024-05-05 14:26:21 +0100

    Limitação API Pais

--- app/api/pais/create/route.ts
+++ app/api/pais/create/route.ts
@@ -1,7 +1,24 @@
-import prisma from '@/lib/prisma';
+import prisma from "@/lib/prisma";
 
 export async function POST(req: Request) {
   const { nome, gentilico } = await req.json();
+
+  const existe = await prisma.pais.findFirst({
+    where: {
+      pais: {
+        equals: nome,
+        mode: "insensitive",
+      },
+    },
+  });
+
+  if (existe) {
+    return new Response("Unidade já existe", {
+      status: 409,
+      statusText: "Unidade já existe",
+    });
+  }
+
   const result = await prisma.pais.create({
     data: {
       pais: nome,
