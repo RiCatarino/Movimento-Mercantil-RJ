8a7d4b2a676ccb0687a447f7d47d29ea9e179278
jose.carreira <jlncarreira@gmail.com>
2024-05-12 20:02:34 +0100

    changelog 1.1.24

---
+++ app/api/user/create/signout/route.ts
@@ -0,0 +1,24 @@
+import { lucia, validateRequest } from "@/auth";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function GET() {
+  const { session } = await validateRequest();
+  if (!session) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+
+  return new Response("OK", {
+    status: 200,
+    statusText: "OK",
+  });
+}
