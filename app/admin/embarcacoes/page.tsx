2e5af6a79df671daef9b458a0f83b1fbe470a17b
Antonio Palma <antoniopalma9999@gmail.com>
2024-05-12 18:10:25 +0100

    new branch

--- app/admin/embarcacoes/page.tsx
+++ app/admin/embarcacoes/page.tsx
@@ -1,23 +1,23 @@
-import { TableEmbarcacoes } from './_components/embarcacoes/table';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { TableTipos } from './_components/tipos/table';
+import { TableEmbarcacoes } from "./_components/embarcacoes/table";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { TableTipos } from "./_components/tipos/table";
 
 export default function EmbarcacoesPage() {
   return (
-    <main className='p-4 md:mx-24 '>
-      <Tabs defaultValue='embarcacoes' className='w-full'>
-        <TabsList className='rounded-3xl bg-blue-200 w-full'>
-          <TabsTrigger value='embarcacoes' className='rounded-3xl w-1/2 '>
+    <main className="p-4 md:mx-24 ">
+      <Tabs defaultValue="embarcacoes" className="w-full">
+        <TabsList className="rounded-3xl bg-blue-200 w-full">
+          <TabsTrigger value="embarcacoes" className="rounded-3xl w-1/2 ">
             Embarcações
           </TabsTrigger>
-          <TabsTrigger value='tipos' className='rounded-3xl w-1/2'>
+          <TabsTrigger value="tipos" className="rounded-3xl w-1/2">
             Tipos de Embarcações
           </TabsTrigger>
         </TabsList>
-        <TabsContent value='embarcacoes'>
+        <TabsContent value="embarcacoes">
           <TableEmbarcacoes />
         </TabsContent>
-        <TabsContent value='tipos'>
+        <TabsContent value="tipos">
           <TableTipos />
         </TabsContent>
       </Tabs>
