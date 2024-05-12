9cb5fb40ab3a5146a0a3ea8bf16fe6cd1e2343c5
jose.carreira <jlncarreira@gmail.com>
2024-05-12 15:47:34 +0100

    last one

--- app/api/porto/create/route.ts
+++ app/api/porto/create/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 
 export async function POST(req: Request) {
-  const { nome, id_pais, pais } = await req.json();
+  const { nome, id_pais } = await req.json();
 
   const existe = await prisma.porto.findFirst({
     where: {
