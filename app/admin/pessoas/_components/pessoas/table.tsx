09f93bebcbb4e48ad28fba96ec85f9262d8ec897
Ricardo Catarino <ricardomicc@gmail.com>
2024-05-14 20:51:31 +0100

    test for swr bug

--- app/admin/pessoas/_components/pessoas/table.tsx
+++ app/admin/pessoas/_components/pessoas/table.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import {
   Table,
   TableBody,
@@ -6,43 +6,44 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
+} from '@/components/ui/table';
 // import VesselDetails from './vesseldetails';
-import { useState } from "react";
-import PersonDetails from "./persondetails";
-import fetcher from "@/lib/fetch";
-import Loader from "@/components/loader";
-import useSWR from "swr";
-import NewPerson from "./buttonnew";
-import Paginacao from "@/components/sharedpagination";
-import chunk from "@/lib/chunk";
+import { useState } from 'react';
+import PersonDetails from './persondetails';
+import fetcher from '@/lib/fetch';
+import Loader from '@/components/loader';
+import useSWR from 'swr';
+import NewPerson from './buttonnew';
+import Paginacao from '@/components/sharedpagination';
+import chunk from '@/lib/chunk';
 
 export function PeopleTable() {
   const [activePage, setPage] = useState(1);
   const [open, setOpen] = useState(false);
   const [pessoa_id, setPessoaId] = useState<number | undefined>();
+
   const {
     data: pessoasdata,
     isLoading,
     mutate,
-  } = useSWR<Pessoa[]>("/api/pessoa/read", fetcher);
+  } = useSWR<Pessoa[]>('/api/pessoa/read', fetcher, {});
 
   const chunked = chunk(pessoasdata ?? [], 10);
   const pessoas = chunked[activePage - 1];
 
   if (isLoading)
     return (
-      <main className="flex flex-row justify-center p-4">
-        <Loader classProp="w-24 h-24 self-center flex" />
+      <main className='flex flex-row justify-center p-4'>
+        <Loader classProp='w-24 h-24 self-center flex' />
       </main>
     );
 
   return (
-    <div className="flex flex-col  gap-2 mt-2 p-2 border-2 border-gray-300 border-solid shadow-lg rounded-3xl">
+    <div className='flex flex-col  gap-2 mt-2 p-2 border-2 border-gray-300 border-solid shadow-lg rounded-3xl'>
       <NewPerson mutate={mutate} />
       <Table>
-        <TableHeader className="p-2 text-xs border-t-0 bg-gradient-to-r from-blue-200 to-blue-400 ">
-          <TableRow className="rounded-ss-xl">
+        <TableHeader className='p-2 text-xs border-t-0 bg-gradient-to-r from-blue-200 to-blue-400 '>
+          <TableRow className='rounded-ss-xl'>
             <TableHead>ID</TableHead>
             <TableHead>Nome</TableHead>
             <TableHead>Título Nobreza</TableHead>
@@ -52,7 +53,7 @@ export function PeopleTable() {
         <TableBody>
           {pessoas?.map((pessoa) => (
             <TableRow
-              className="cursor-pointer hover:bg-blue-100"
+              className='cursor-pointer hover:bg-blue-100'
               key={pessoa.id}
               onClick={(e) => {
                 e.stopPropagation();
@@ -60,14 +61,14 @@ export function PeopleTable() {
                 setOpen(true);
               }}
             >
-              <TableCell className="text-xs font-medium">{pessoa.id}</TableCell>
-              <TableCell className="text-xs font-medium">
+              <TableCell className='text-xs font-medium'>{pessoa.id}</TableCell>
+              <TableCell className='text-xs font-medium'>
                 {pessoa.nome}
               </TableCell>
-              <TableCell className="text-xs font-medium">
+              <TableCell className='text-xs font-medium'>
                 {pessoa?.titulo_nobreza?.titulo}
               </TableCell>
-              <TableCell className="text-xs font-medium">
+              <TableCell className='text-xs font-medium'>
                 {pessoa.pais?.pais}
               </TableCell>
             </TableRow>
