1754f5360f511e3a8215b02e349a3d7079cf3bf7
Ricardo Catarino <ricardomicc@gmail.com>
2024-05-18 15:43:04 +0100

    reset password done

--- app/api/user/update/resetpassword/route.ts
+++ app/api/user/update/resetpassword/route.ts
@@ -1,20 +1,20 @@
-import { validateRequest } from "@/auth";
-import prisma from "@/lib/prisma";
-import sendEmailNotification from "@/lib/sendemail";
-import { hash } from "@node-rs/argon2";
+import { lucia, validateRequest } from '@/auth';
+import prisma from '@/lib/prisma';
+import sendEmailNotification from '@/lib/sendemail';
+import { hash } from '@node-rs/argon2';
 
 export async function PUT(req: Request) {
   const { id } = await req.json();
   const { user } = await validateRequest();
 
-  if (!user || user.role !== "ADMIN") {
-    return new Response("Unauthorized", { status: 401 });
+  if (!user || user.role !== 'ADMIN') {
+    return new Response('Unauthorized', { status: 401 });
   }
 
   function generatePass() {
-    let pass = "";
+    let pass = '';
     let str =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$";
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz0123456789@#$';
 
     for (let i = 1; i <= 16; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
@@ -35,6 +35,8 @@ export async function PUT(req: Request) {
     parallelism: 1,
   });
 
+  await lucia.invalidateUserSessions(id);
+
   const result = await prisma.user.update({
     where: {
       id: id,
@@ -48,12 +50,12 @@ export async function PUT(req: Request) {
   });
   await sendEmailNotification(
     result.email,
-    "Um Administrador resetou a sua senha.",
+    'Um Administrador resetou a sua senha.',
     `${password}`
   );
 
-  return new Response("Senha resetada com sucesso.", {
+  return new Response('Senha resetada com sucesso.', {
     status: 201,
-    statusText: "Senha resetada com sucesso.",
+    statusText: 'Senha resetada com sucesso.',
   });
 }
