6fd725277dd26818dd5397a9941b74987787c56d
Ricardo Catarino <ricardomicc@gmail.com>
2024-05-18 15:52:03 +0100

    fixed block user

--- app/api/user/update/block/route.ts
+++ app/api/user/update/block/route.ts
@@ -1,13 +1,14 @@
-import { validateRequest } from "@/auth";
-import prisma from "@/lib/prisma";
+import { lucia, validateRequest } from '@/auth';
+import prisma from '@/lib/prisma';
 
 export async function PUT(req: Request) {
   const { id, habilitado } = await req.json();
   const { user } = await validateRequest();
 
-  if (!user || user.role !== "ADMIN") {
-    return new Response("Unauthorized", { status: 401 });
+  if (!user || user.role !== 'ADMIN') {
+    return new Response('Unauthorized', { status: 401 });
   }
+  await lucia.invalidateUserSessions(id);
 
   const result = await prisma.user.update({
     where: {
