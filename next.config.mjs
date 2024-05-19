6e462c96d94d3362b180c51adc0cb591b1f2837b
Ricardo Catarino <ricardomicc@gmail.com>
2024-05-19 01:50:53 +0100

    main page fix layout, about page created and some stuff put inside

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@node-rs/argon2'],
   },
+  images: {
+    domains: [
+      'localhost',
+      'avatars.githubusercontent.com',
+      'lh6.googleusercontent.com',
+    ],
+  },
 };
-
 export default nextConfig;
