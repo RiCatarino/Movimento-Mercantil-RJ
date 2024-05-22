f1c71949cb3d4ac599655a04a59995384f01c301
jose.carreira <jlncarreira@gmail.com>
2024-05-22 17:24:55 +0100

    adicionado email para reportar bugs

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { motion } from 'framer-motion';
-import dynamic from 'next/dynamic';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import React from "react";
+import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const World = dynamic(
-  () => import('@/components/ui/globe').then((m) => m.World),
+  () => import("@/components/ui/globe").then((m) => m.World),
   {
     ssr: false,
   }
@@ -17,18 +17,18 @@ const World = dynamic(
 function Globe() {
   const globeConfig = {
     pointSize: 10,
-    globeColor: '#84CBF9',
+    globeColor: "#84CBF9",
     showAtmosphere: false,
-    atmosphereColor: '#000',
+    atmosphereColor: "#000",
     atmosphereAltitude: 0.1,
-    emissive: '#062056',
+    emissive: "#062056",
     emissiveIntensity: 0.1,
     shininess: 0.9,
-    polygonColor: '#BF1222',
-    ambientLight: '#38bdf8',
-    directionalLeftLight: '#fff',
-    directionalTopLight: '#fff',
-    pointLight: '#000',
+    polygonColor: "#BF1222",
+    ambientLight: "#38bdf8",
+    directionalLeftLight: "#fff",
+    directionalTopLight: "#fff",
+    pointLight: "#000",
     arcTime: 1000,
     arcLength: 0.9,
     rings: 1,
@@ -37,7 +37,7 @@ function Globe() {
     autoRotate: true,
     autoRotateSpeed: 0.1,
   };
-  const colors = ['#06b6d4', '#3b82f6', '#6366f1'];
+  const colors = ["#06b6d4", "#3b82f6", "#6366f1"];
   const sampleArcs = [
     {
       order: 1,
@@ -403,12 +403,12 @@ function Globe() {
 
   return (
     // <div className='relative z-0 flex flex-row items-center justify-center py-20 md:h-auto dark:bg-black w-fit'>
-    <div className=' lg:max-w-[50%] w-full  relative overflow-hidden  md:h-[40rem]  '>
+    <div className=" lg:max-w-[50%] w-full  relative overflow-hidden  md:h-[40rem]  ">
       <div>
         {/* <div className='relative inset-x-0 bottom-0 z-40 h-40 pointer-events-none select-none w-fit bg-gradient-to-b from-transparent dark:to-black to-white' /> */}
         <div
-          className='z-10 w-full cursor-pointer lg:absolute -bottom-5 h-72 md:h-full drop-shadow-xl hover:scale-105 transition-all duration-300 ease-in-out
- '
+          className="z-10 w-full cursor-pointer lg:absolute -bottom-5 h-72 md:h-full drop-shadow-xl hover:scale-105 transition-all duration-300 ease-in-out
+ "
         >
           <World data={sampleArcs} globeConfig={globeConfig} />
         </div>
@@ -419,8 +419,8 @@ function Globe() {
 
 export default function Home() {
   return (
-    <main className='flex max-h-screen '>
-      <div className='z-10 flex flex-wrap-reverse items-center justify-center w-full  dark:bg-black md:h-screen md:mt-0 mt-52'>
+    <main className="flex max-h-screen ">
+      <div className="z-10 flex flex-wrap-reverse items-center justify-center w-full  dark:bg-black md:h-screen md:mt-0 mt-52">
         <Globe />
         <motion.div
           initial={{
@@ -434,31 +434,32 @@ export default function Home() {
           transition={{
             duration: 1,
           }}
-          className='div'
+          className="div"
         >
-          <h1 className='text-4xl text-red-500 font-extrabold text-center bg-white p-2 rounded-xl'>
+          <h1 className="text-4xl text-red-500 font-extrabold text-center bg-white p-2 rounded-xl">
             Este site encontra-se em desenvolvimento.
+            <p> Report de bugs e sugestões para: 30008236@students.ual.pt </p>
           </h1>
-          <h2 className='text-xl font-bold text-center text-black md:text-4xl dark:text-white'>
+          <h2 className="text-xl font-bold text-center text-black md:text-4xl dark:text-white">
             Viagens pelo globo, a partir do Rio.
           </h2>
-          <p className='max-w-md mx-auto mt-2 text-base font-normal text-center md:text-lg text-neutral-700 dark:text-neutral-200'>
+          <p className="max-w-md mx-auto mt-2 text-base font-normal text-center md:text-lg text-neutral-700 dark:text-neutral-200">
             Universidade Autónoma de Lisboa
           </p>
 
           <Image
-            src='/logocircuitosoceanicos.png'
-            className='mx-auto mt-4 bg-cover rounded-full hover:rotate-12 hover:scale-105 hover:shadow-2xl transition-all duration-300 ease-in-out'
+            src="/logocircuitosoceanicos.png"
+            className="mx-auto mt-4 bg-cover rounded-full hover:rotate-12 hover:scale-105 hover:shadow-2xl transition-all duration-300 ease-in-out"
             width={200}
             height={200}
-            alt='logo'
+            alt="logo"
           />
-          <div className='flex justify-center p-3' style={{}}>
+          <div className="flex justify-center p-3" style={{}}>
             <Button
               asChild
-              className='self-end w-full text-lg shadow-2xl h-14 transition-all duration-500 bg-gradient-to-r from-blue-400 to-blue-600 rounded-xl md:w-fit hover:scale-105 hover:bg-gradient-to-l hover:from-blue-400 hover:to-blue-600'
+              className="self-end w-full text-lg shadow-2xl h-14 transition-all duration-500 bg-gradient-to-r from-blue-400 to-blue-600 rounded-xl md:w-fit hover:scale-105 hover:bg-gradient-to-l hover:from-blue-400 hover:to-blue-600"
             >
-              <Link href={'/admin/dashboard'}>Visitar BackOffice</Link>
+              <Link href={"/admin/dashboard"}>Visitar BackOffice</Link>
             </Button>
           </div>
         </motion.div>
@@ -466,86 +467,86 @@ export default function Home() {
       </div>
       <div
         style={{
-          position: 'absolute',
-          width: '100%',
+          position: "absolute",
+          width: "100%",
         }}
-        className='z-0 mb-0 pointer-events-none '
+        className="z-0 mb-0 pointer-events-none "
       >
         <svg
-          version='1.1'
-          xmlns='http://www.w3.org/2000/svg'
-          xmlnsXlink='http://www.w3.org/1999/xlink'
-          x='0px'
-          y='0px'
-          width='100%'
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          width="100%"
           // height='100vh'
-          className='h-screen'
-          viewBox='0 0 1600 900'
-          preserveAspectRatio='xMidYMax slice'
+          className="h-screen"
+          viewBox="0 0 1600 900"
+          preserveAspectRatio="xMidYMax slice"
         >
           <defs>
-            <linearGradient id='bg'>
+            <linearGradient id="bg">
               <stop
-                offset='0%'
+                offset="0%"
                 style={{
-                  stopColor: 'rgba(130, 158, 249, 0.06)',
+                  stopColor: "rgba(130, 158, 249, 0.06)",
                 }}
               ></stop>
               <stop
-                offset='50%'
-                style={{ stopColor: 'rgba(76, 190, 255, 0.6)' }}
+                offset="50%"
+                style={{ stopColor: "rgba(76, 190, 255, 0.6)" }}
               ></stop>
 
               <stop
-                offset='100%'
-                style={{ stopColor: 'rgba(10, 10, 72, 0.2)' }}
+                offset="100%"
+                style={{ stopColor: "rgba(10, 10, 72, 0.2)" }}
               ></stop>
             </linearGradient>
             <path
-              id='wave'
-              fill='url(#bg)'
-              d='M-363.852,502.589c0,0,236.988-41.997,505.475,0
-	s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z'
+              id="wave"
+              fill="url(#bg)"
+              d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
+	s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z"
             />
           </defs>
           <g>
-            <use xlinkHref='#wave' opacity='.3'>
+            <use xlinkHref="#wave" opacity=".3">
               <animateTransform
-                attributeName='transform'
-                attributeType='XML'
-                type='translate'
-                dur='10s'
-                calcMode='spline'
-                values='270 230; -334 180; 270 230'
-                keyTimes='0; .5; 1'
-                keySplines='0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0'
-                repeatCount='indefinite'
+                attributeName="transform"
+                attributeType="XML"
+                type="translate"
+                dur="10s"
+                calcMode="spline"
+                values="270 230; -334 180; 270 230"
+                keyTimes="0; .5; 1"
+                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                repeatCount="indefinite"
               />
             </use>
-            <use xlinkHref='#wave' opacity='.6'>
+            <use xlinkHref="#wave" opacity=".6">
               <animateTransform
-                attributeName='transform'
-                attributeType='XML'
-                type='translate'
-                dur='8s'
-                calcMode='spline'
-                values='-270 230;243 220;-270 230'
-                keyTimes='0; .6; 1'
-                keySplines='0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0'
-                repeatCount='indefinite'
+                attributeName="transform"
+                attributeType="XML"
+                type="translate"
+                dur="8s"
+                calcMode="spline"
+                values="-270 230;243 220;-270 230"
+                keyTimes="0; .6; 1"
+                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                repeatCount="indefinite"
               />
             </use>
-            <use xlinkHref='#wave' opacity='.9'>
+            <use xlinkHref="#wave" opacity=".9">
               <animateTransform
-                attributeName='transform'
-                attributeType='XML'
-                type='translate'
-                dur='6s'
-                calcMode='spline'
-                values='0 230;-140 200;0 230'
-                keyTimes='0; .4; 1'
-                keySplines='0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0'
-                repeatCount='indefinite'
+                attributeName="transform"
+                attributeType="XML"
+                type="translate"
+                dur="6s"
+                calcMode="spline"
+                values="0 230;-140 200;0 230"
+                keyTimes="0; .4; 1"
+                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                repeatCount="indefinite"
               />
             </use>
           </g>
