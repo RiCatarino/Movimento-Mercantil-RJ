5927f6b4fcf962cde019b72708a6c0146a2d0fdf
jose.carreira <jlncarreira@gmail.com>
2024-06-08 13:03:31 +0100

    changelog 0.1.55

--- app/api/embarcacao/read/route.ts
+++ app/api/embarcacao/read/route.ts
@@ -2,12 +2,14 @@ import { validateRequest } from "@/auth";
 import prism from "@/lib/prisma";
 
 export const dynamic = "force-dynamic";
-export async function GET() {
+export async function GET(req: Request) {
   const { user } = await validateRequest();
 
   if (!user) {
     return new Response("Unauthorized", { status: 401 });
   }
+  const { searchParams } = new URL(req.url);
+  const page = searchParams.get("page");
 
   const result = await prism.embarcacao.findMany({
     select: {
@@ -31,6 +33,7 @@ export async function GET() {
       nome: "asc",
     },
     take: 10,
+    skip: page ? 10 * (+page - 1) : 0,
   });
   return Response.json(result);
 }
--- app/api/embarcacao/read/tipos/route.ts
+++ app/api/embarcacao/read/tipos/route.ts
@@ -2,12 +2,14 @@ import { validateRequest } from "@/auth";
 import prism from "@/lib/prisma";
 
 export const dynamic = "force-dynamic";
-export async function GET() {
+export async function GET(req: Request) {
   const { user } = await validateRequest();
 
   if (!user) {
     return new Response("Unauthorized", { status: 401 });
   }
+  const { searchParams } = new URL(req.url);
+  const page = searchParams.get("page");
 
   const result = await prism.tipo_embarcacao.findMany({
     select: {
@@ -21,6 +23,8 @@ export async function GET() {
         },
       },
     },
+    take: 10,
+    skip: page ? 10 * (+page - 1) : 0,
   });
   return Response.json(result);
 }
