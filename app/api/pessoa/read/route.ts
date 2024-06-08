73e158cb3a99d08b04a765ea65690f3630347c73
jose.carreira <jlncarreira@gmail.com>
2024-06-08 13:03:31 +0100

    changelog 0.1.55

--- app/api/pessoa/read/route.ts
+++ app/api/pessoa/read/route.ts
@@ -3,12 +3,14 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic"; // needed because of mutations
 
-export async function GET() {
+export async function GET(req: Request) {
   const { user } = await validateRequest();
 
   if (!user) {
     return new Response("Unauthorized", { status: 401 });
   }
+  const { searchParams } = new URL(req.url);
+  const page = searchParams.get("page");
 
   const result = await prisma.pessoa.findMany({
     select: {
@@ -31,7 +33,8 @@ export async function GET() {
         },
       },
     },
-    take: 100,
+    take: 10,
+    skip: page ? 10 * (+page - 1) : 0,
 
     orderBy: {
       id: "desc",
--- app/api/porto/read/route.ts
+++ app/api/porto/read/route.ts
@@ -2,12 +2,14 @@ import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
 
 export const dynamic = "force-dynamic";
-export async function GET() {
+export async function GET(req: Request) {
   const { user } = await validateRequest();
 
   if (!user) {
     return new Response("Unauthorized", { status: 401 });
   }
+  const { searchParams } = new URL(req.url);
+  const page = searchParams.get("page");
 
   const result = await prisma.porto.findMany({
     select: {
@@ -18,6 +20,8 @@ export async function GET() {
     orderBy: {
       nome: "asc",
     },
+    take: 10,
+    skip: page ? 10 * (+page - 1) : 0,
   });
   return Response.json(result);
 }
