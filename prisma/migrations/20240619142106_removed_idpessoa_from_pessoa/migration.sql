6453d57467bd4208119979ab08a9c8dc9716d149
Ricardo Catarino <ricardomicc@gmail.com>
2024-06-20 12:08:27 +0100

    changed db stuff

---
+++ prisma/migrations/20240619142106_removed_idpessoa_from_pessoa/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `id_pessoa` on the `pessoa` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "pessoa" DROP COLUMN "id_pessoa";
