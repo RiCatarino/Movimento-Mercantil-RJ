4d9809069d837410b9b2a4c40f7f04ecb506ab24
Antonio Palma <antoniopalma9999@gmail.com>
2024-06-27 16:09:37 +0100

    bug fixes

--- app/(main)/dados/viagens/_components/tableescalas.tsx
+++ app/(main)/dados/viagens/_components/tableescalas.tsx
@@ -7,41 +7,35 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import tz from "dayjs/plugin/timezone";
 import { useState } from "react";
-import EscalasDrawer from "./mercadoriaescaladrawer";
-import { XIcon } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import Loader from "@/components/loader";
+import Paginacao from "@/components/sharedpagination";
+import chunk from "@/lib/chunk";
+import MercadoriaEscalasDrawer from "./mercadoriaescaladrawer";
+
+dayjs.extend(utc);
+dayjs.extend(tz);
 
 export default function TableEscalas(props: {
   escalas: Escala[] | undefined;
   mutate: () => void;
 }) {
+  const [activePage, setPage] = useState(1);
   const { escalas, mutate } = props;
   const [open, setOpen] = useState(false);
-  const [deleting, setDeleting] = useState(false);
   const [relacmercadoriaescala, setRelacMercadoriaEscala] = useState<
     RelacMercadoriaEscala[] | undefined
   >();
+  const [escala_id, setEscalaId] = useState<number | undefined>();
 
-  async function handleDeleteEscala(id: number) {
-    setDeleting(true);
-    await fetch(`/api/escala/delete`, {
-      method: "DELETE",
-      body: JSON.stringify({ id }),
-    });
-    mutate();
-    setDeleting(false);
-  }
-
-  if (deleting) {
-    return <Loader classProp="w-10 h-10" />;
-  }
+  const chunked = chunk(escalas ?? [], 5);
+  const escalasdata = chunked[activePage - 1];
 
   return (
     <>
       <Table className="border-b">
-        <TableHeader className="p-2 text-xs bg-blue-200 border-t-0 ">
+        <TableHeader className="p-2 text-xs bg-blue-200 border-t-0 dark:bg-slate-900 ">
           <TableRow className="rounded-ss-xl">
             <TableHead>Data</TableHead>
             <TableHead>Ano</TableHead>
@@ -49,21 +43,21 @@ export default function TableEscalas(props: {
             <TableHead>Porto</TableHead>
             <TableHead>Entraram</TableHead>
             <TableHead>Saíram</TableHead>
-            <TableHead></TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {escalas?.map((escala) => (
+          {escalasdata?.map((escala) => (
             <TableRow
               className="cursor-pointer hover:bg-blue-100"
               key={escala.id}
               onClick={(e) => {
                 setRelacMercadoriaEscala(escala.relac_mercadoria_escala);
+                setEscalaId(escala.id);
                 setOpen(true);
               }}
             >
               <TableCell className="text-xs font-medium">
-                {dayjs(escala.data_escala).format("DD-MM-YYYY")}
+                {dayjs.tz(escala.data_escala, "UTC").format("DD/MM/YYYY")}
               </TableCell>
               <TableCell className="text-xs font-medium">
                 {escala.ano}
@@ -71,7 +65,7 @@ export default function TableEscalas(props: {
               <TableCell className="text-xs font-medium">
                 {escala.dias_porto}
               </TableCell>
-              <TableCell className="text-xs font-medium">
+              <TableCell className="text-xs font-medium ">
                 {escala.porto.nome}
               </TableCell>
               <TableCell className="text-xs font-medium">
@@ -80,27 +74,15 @@ export default function TableEscalas(props: {
               <TableCell className="text-xs font-medium">
                 {escala.saida_de_passageiros}
               </TableCell>
-              <TableCell className="w-4">
-                <Button
-                  variant="link"
-                  size="icon"
-                  className="rounded-full"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    handleDeleteEscala(escala.id);
-                  }}
-                >
-                  <XIcon className="w-4 text-red-600" />
-                </Button>
-              </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-      <EscalasDrawer
+      <Paginacao chunked={chunked} activePage={activePage} setPage={setPage} />
+      <MercadoriaEscalasDrawer
         open={open}
         setOpen={setOpen}
-        relac_mercadoria_escala={relacmercadoriaescala}
+        escala_id={escala_id}
       />
     </>
   );
