e9703f95624e2acf545c9e819fa7a9fddcf4e311
Ricardo Catarino <ricardomicc@gmail.com>
2024-06-30 15:54:54 +0100

    some changes

--- app/(admin)/admin/viagens/_components/tripdetails.tsx
+++ app/(admin)/admin/viagens/_components/tripdetails.tsx
@@ -1,7 +1,6 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog';
@@ -24,7 +23,6 @@ import { toast } from 'sonner';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import tz from 'dayjs/plugin/timezone';
-
 import {
   Accordion,
   AccordionContent,
