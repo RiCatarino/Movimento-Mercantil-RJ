8d1c939d3407fbd6ec12c14beda700535ae0d1bd
Ricardo Catarino <ricardomicc@gmail.com>
2024-06-30 10:29:48 +0100

    check changelog 0.1.63

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,10 +1,16 @@
-import { DocsLayout } from "fumadocs-ui/layout";
-import { pageTree } from "@/app/source";
-import type { ReactNode } from "react";
+import { DocsLayout } from 'fumadocs-ui/layout';
+import { pageTree } from '@/app/source';
+import type { ReactNode } from 'react';
 
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={pageTree} nav={{ title: "Documentação" }}>
+    <DocsLayout
+      tree={pageTree}
+      nav={{ title: 'Documentação' }}
+      sidebar={{
+        defaultOpenLevel: 0,
+      }}
+    >
       {children}
     </DocsLayout>
   );
