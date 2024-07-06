e8e0f20a34bc5ccbc635f078d08a6d21b58d5656
Ricardo Catarino <ricardomicc@gmail.com>
2024-07-06 11:27:08 +0100

    typo

--- app/(admin)/admin/unidades_de_medida/_components/editdialog.tsx
+++ app/(admin)/admin/unidades_de_medida/_components/editdialog.tsx
@@ -1,35 +1,35 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+} from '@/components/ui/dialog';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormLabel,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { SetStateAction, useEffect, useState } from "react";
-import Loader from "@/components/loader";
-import { useToast } from "@/components/ui/use-toast";
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { SetStateAction, useEffect, useState } from 'react';
+import Loader from '@/components/loader';
+import { useToast } from '@/components/ui/use-toast';
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
+} from '@/components/ui/select';
 
 const formSchema = z.object({
   id: z.number(),
-  unidade: z.string().min(1, { message: "Nome muito curto" }),
+  unidade: z.string().min(1, { message: 'Nome muito curto' }),
 });
 
 export default function DialogEditarUnidadeMedida(props: {
@@ -61,8 +61,8 @@ export default function DialogEditarUnidadeMedida(props: {
 
   async function handleSubmit(values: z.infer<typeof formSchema>) {
     setSubmitting(true);
-    const result = await fetch("/api/unidade_de_medida/update", {
-      method: "PUT",
+    const result = await fetch('/api/unidade_de_medida/update', {
+      method: 'PUT',
       body: JSON.stringify(values),
     });
 
@@ -71,17 +71,17 @@ export default function DialogEditarUnidadeMedida(props: {
       form.reset();
       mutate();
       toast({
-        className: "bg-green-200",
-        title: "Sucesso",
+        className: 'bg-green-200',
+        title: 'Sucesso',
         duration: 5000,
-        description: "Unidade de medida editada com sucesso",
+        description: 'Unidade de medida editada com sucesso',
       });
     } else {
       toast({
-        variant: "destructive",
-        title: "Erro",
+        variant: 'destructive',
+        title: 'Erro',
         duration: 5000,
-        description: "Erro ao editar unidade de medida",
+        description: 'Erro ao editar unidade de medida',
       });
     }
     setSubmitting(false);
@@ -95,39 +95,41 @@ export default function DialogEditarUnidadeMedida(props: {
     >
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent
-          className=" w-11/12 p-6 rounded-lg max-h-[95%] overflow-y-scroll"
+          className=' w-11/12 p-6 rounded-lg max-h-[95%] overflow-y-scroll'
           onClick={(e) => {
             e.stopPropagation();
           }}
         >
           <DialogHeader>
-            <DialogTitle className="text-blue-500">Editar Usuário</DialogTitle>
+            <DialogTitle className='text-blue-500'>
+              Editar Unidade de Medida
+            </DialogTitle>
           </DialogHeader>
           <Form {...form}>
             <form
               onSubmit={form.handleSubmit(handleSubmit)}
-              className="flex flex-col gap-2"
+              className='flex flex-col gap-2'
             >
               <FormField
                 control={form.control}
-                name="unidade"
+                name='unidade'
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Nome</FormLabel>
                     <FormControl>
-                      <Input className="rounded-xl" {...field} />
+                      <Input className='rounded-xl' {...field} />
                     </FormControl>
                   </FormItem>
                 )}
               />
               <Button
-                type="submit"
-                className="self-end mt-2 bg-blue-500 rounded-2xl hover:bg-blue-600 w-fit"
+                type='submit'
+                className='self-end mt-2 bg-blue-500 rounded-2xl hover:bg-blue-600 w-fit'
                 disabled={
                   submitting || form.watch().unidade == unidade?.unidade_medida
                 }
               >
-                Editar {submitting && <Loader classProp="ml-2 w-6 h-6" />}
+                Editar {submitting && <Loader classProp='ml-2 w-6 h-6' />}
               </Button>
             </form>
           </Form>
